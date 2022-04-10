ab026b62bbbc06ecd7920f3e3809589d82ea9219
giordi96 <fedegiordani2@gmail.com>
2022-04-10 21:35:19 +0200

    SVG added - Added the option to generate barcodes as .svg files (first save option) - Removed text below the barcode - Save window now opens the first time in .exe directory, then in the folder in which the previous barcode has been saved

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+from barcode import generate
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QFileDialog, QWidget
@@ -23,35 +24,56 @@ class BGGui(QWidget):
         self.icon = QIcon(str(get_resource_path(r"res/icon.png")))
         self.setWindowIcon(self.icon)
 
+        self.save_path = None
         self.button_salva.clicked.connect(self.generate_barcode)
 
     def generate_barcode(self) -> None:
         try:
             options = QFileDialog.Options()
             options |= QFileDialog.DontUseNativeDialog
-            file_name, filter = QFileDialog.getSaveFileName(self,
-                                                            "Save file",
-                                                            "",
-                                                            "PNG (*.png);;PDF (*.pdf)",
-                                                            options=options)
+            file_name, filter = QFileDialog.getSaveFileName(
+                parent=self,
+                caption="Save file",
+                directory=self.save_path if self.save_path != None else "",
+                filter="SVG (*.svg);;PNG (*.png);;PDF (*.pdf)",
+                options=options)
+
+            # refreshes the starting file dialog directory to the last selected folder
+            try:
+                self.save_path = file_name[:file_name.rfind("\\")]
+            except:
+                pass
+
+            # formats input code by inserting \t and \n characters
+            code = self.input_testo.text()
+            code = code.replace("\\n", "\n")
+            code = code.replace("\\t", "\t")
+
             if file_name:
 
-                if filter.startswith("PNG"):
-                    format = "PNG"
-                    if not file_name.endswith(".png"):
-                        file_name = ".".join((file_name, "png"))
+                if filter.startswith("SVG"):
 
-                elif filter.startswith("PDF"):
-                    format = "PDF"
-                    if not file_name.endswith(".pdf"):
-                        file_name = ".".join((file_name, "pdf"))
+                    generate(name='CODE128',
+                            code=code,
+                            output=file_name,
+                            writer_options={"write_text": False})
 
-                code = self.input_testo.text()
-                code = code.replace("\\n", "\n")
-                code = code.replace("\\t", "\t")
+                else:
 
-                with open(f"{file_name}", 'wb') as file:
-                    Code128(code=code,
-                            writer=ImageWriter(format=format)).write(file)
+                    if filter.startswith("PNG"):
+                        format = "PNG"
+                        if not file_name.endswith(".png"):
+                            file_name = ".".join((file_name, "png"))
+
+                    elif filter.startswith("PDF"):
+                        format = "PDF"
+                        if not file_name.endswith(".pdf"):
+                            file_name = ".".join((file_name, "pdf"))
+
+                    with open(f"{file_name}", 'wb') as file:
+                        Code128(code=code,
+                                writer=ImageWriter(format=format)
+                                ).write(file,
+                                        options={"write_text": False})
         except Exception as e:
             self.input_testo.setText(f"Errore: {str(e)}")
